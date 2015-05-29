7173dabb939964098cf9432c48110029b9db70cd
Yanzhi_Chen_1011447 <cng1698@gmail.com>
2015-05-29 22:48:12 +0800

    Login password output is '*'

---
+++ team4/Login_Logout.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team4
+{
+    class Login_Logout
+    {
+        static void Login()
+        {
+            string username;
+            string password;
+            Console.WriteLine("Username: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = loadpassword();
+            Console.WriteLine(password);
+            Console.ReadLine();
+        }
+        static string loadpassword()
+        {
+            string password = "";
+            ConsoleKeyInfo input = Console.ReadKey(true);
+            while (input.Key != ConsoleKey.Enter)
+            {
+                if(input.Key != ConsoleKey.Backspace)
+                {
+                    Console.WriteLine("*");
+                    password += input.KeyChar;
+                }
+                else
+                {
+                    password = password.Substring(0, password.Length - 1);
+                }
+            }
+            return password;
+        }
+    }
+}
