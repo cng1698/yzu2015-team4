8b20fd26d382f011a9d01699b0c6d793c987cda0
Jerry <l45632100@hotmail.com>
2015-05-29 21:32:42 +0800

    email version 1

---
+++ team4/Emailtest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace team4
+{
+    [TestClass]
+    public class Emailtest
+    {
+        [TestMethod]
+        public void ()
+        {
+
+
+        }
+    }
+}
