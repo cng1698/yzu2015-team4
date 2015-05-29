8a334c3ef05170e70725ac0be7364f37eb94f1e6
Jerry <l45632100@hotmail.com>
2015-05-30 02:28:19 +0800

    email version 2

--- team4/Email_Varify.cs
+++ team4/Email_Varify.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace team4
 {
+
+    public struct account_varify
+    {
+        public string account, verification_code;
+
+        public account_varify(string a, string b)
+        {
+            account = a;
+            verification_code = b;
+        }
+    }
+
+
     class Email_Varify
     {
+
+        internal static bool verify()
+        {
+            account_varify user = new account_varify();
+            user.account = "Jerry";
+            user.verification_code = produce_code();
+
+            string input = Console.ReadLine();
+
+
+            if( if_same(user.verification_code , input))
+                return true ;
+            else
+                return false ;
+
+        }
+
+        internal static string produce_code()
+        {
+            string code = "abcd";
+
+            /*int i = 0 ;
+            while (i < 4)
+            {
+                code += 
+                i++;
+            }*/
+            return code;
+        }
+
+        internal static bool if_same( string a , string b )
+        {
+            if( a== b )
+                return true ;
+            else 
+                return false;
+        }
     }
 }
--- team4/Emailtest.cs
+++ team4/Emailtest.cs
@@ -7,10 +7,10 @@ namespace team4
     public class Emailtest
     {
         [TestMethod]
-        public void ()
+        public void 驗證碼產生測試()
         {
-
-
+            Assert.AreEqual("abcd",Email_Varify.produce_code());
+            Assert.IsTrue(Email_Varify.if_same("a","a") );
         }
     }
 }
