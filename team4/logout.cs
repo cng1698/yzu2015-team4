5b04eb9f43cbb59d95a383f9732031bad9bfe147
tony <tony.com>
2015-06-05 02:56:50 +0800

    logout

---
+++ team4/logout.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace team4
+{
+    class logoutinput
+    {
+        static public int sure_logout(string  choose)
+        {
+            if (choose == "Y")
+                return 1;
+            else if (choose == "N")
+                return 2;
+            else
+                return 3;
+        }
+
+        static public int logout(int logout,string surelogoutchoose)
+        {
+            int result = 0;
+            if (logout == 1)
+            {
+                if (sure_logout(surelogoutchoose) == 1)
+                    result = 1;
+                if (sure_logout(surelogoutchoose) == 2)
+                    result = 2;
+                if (sure_logout(surelogoutchoose) == 3)
+                    result = 3;
+            }
+            else if (logout == 2)
+            {
+                result = 2;
+            }
+            else
+                result = 4;
+
+            return result;
+        }
+
+    }
+}
