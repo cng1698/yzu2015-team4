3431cb3ada631b6035336731daad0d7aa66492b8
tony <tony.com>
2015-06-05 03:00:36 +0800

    logout-new

--- team4/UnitTest1.cs
+++ team4/UnitTest1.cs
@@ -75,5 +75,31 @@ namespace team4
             Assert.AreEqual(false, team4.Register.CheckEmail(""));
             Assert.AreEqual(false, team4.Register.CheckEmail("12345"));
         }
+
+	// logout
+	[TestMethod]
+            //1:登出
+            //2:不登出
+            //3:選擇登出但問題出在後者選項
+            //4:前者選項有誤
+        public void logoutinput()
+        {   
+            //正常情況
+
+            Assert.AreEqual(1, team4.logoutinput.logout(1, "Y"));//選擇登出並確定
+            Assert.AreEqual(2, team4.logoutinput.logout(1, "N"));//選擇登出卻反悔
+            Assert.AreEqual(2, team4.logoutinput.logout(2, ""));//沒選擇登出
+            Assert.AreEqual(2, team4.logoutinput.logout(2, "Y"));//沒選擇登出，後者選項不跳出
+            Assert.AreEqual(2, team4.logoutinput.logout(2, "N"));//沒選擇登出，後者選項不跳出
+           
+            //非正常情況
+            
+            Assert.AreEqual(3, team4.logoutinput.logout(1, ""));//選擇登出但未確定
+            Assert.AreEqual(3, team4.logoutinput.logout(1, "YFSDFSD"));//選擇登出，但後者選項不正確
+            Assert.AreEqual(4, team4.logoutinput.logout(123, "Y"));//無法判斷是否有按下登出
+            Assert.AreEqual(4, team4.logoutinput.logout(123, "FSDF"));//無法判斷是否有按下登出
+
+        
+        }
     }
 }
