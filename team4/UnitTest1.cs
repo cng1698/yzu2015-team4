2c501968272871295f9b33edc4808ab9ffe73bd0
Yanzhi_Chen_1011447 <cng1698@gmail.com>
2015-06-05 20:53:33 +0800

    Change Login_Logout.cs to Login.cs and add Login test

--- team4/UnitTest1.cs
+++ team4/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace team4
             user temp_user = new user("Tim", "account_for_tim", "Password", 1, "tim@mail.com");
             Assert.IsTrue(team4.database.addUser(temp_user));
             //Assert.IsTrue(true);
-            
+
         }
         [TestMethod]
         public void dbtest_getUser()
@@ -76,14 +76,14 @@ namespace team4
             Assert.AreEqual(false, team4.Register.CheckEmail("12345"));
         }
 
-	// logout
-	[TestMethod]
-            //1:登出
-            //2:不登出
-            //3:選擇登出但問題出在後者選項
-            //4:前者選項有誤
+        // logout
+        [TestMethod]
+        //1:登出
+        //2:不登出
+        //3:選擇登出但問題出在後者選項
+        //4:前者選項有誤
         public void logoutinput()
-        {   
+        {
             //正常情況
 
             Assert.AreEqual(1, team4.logoutinput.logout(1, "Y"));//選擇登出並確定
@@ -91,15 +91,32 @@ namespace team4
             Assert.AreEqual(2, team4.logoutinput.logout(2, ""));//沒選擇登出
             Assert.AreEqual(2, team4.logoutinput.logout(2, "Y"));//沒選擇登出，後者選項不跳出
             Assert.AreEqual(2, team4.logoutinput.logout(2, "N"));//沒選擇登出，後者選項不跳出
-           
+
             //非正常情況
-            
+
             Assert.AreEqual(3, team4.logoutinput.logout(1, ""));//選擇登出但未確定
             Assert.AreEqual(3, team4.logoutinput.logout(1, "YFSDFSD"));//選擇登出，但後者選項不正確
             Assert.AreEqual(4, team4.logoutinput.logout(123, "Y"));//無法判斷是否有按下登出
             Assert.AreEqual(4, team4.logoutinput.logout(123, "FSDF"));//無法判斷是否有按下登出
 
-        
+
+        }
+        [TestMethod]
+        public void Login_test()
+        {
+            //Success
+            Assert.AreEqual("登入成功", Login.Check("account_for_tim", "Password"));
+
+            //空白帳密
+            Assert.AreEqual("帳號及密碼不得為空", Login.Check("", ""));
+            Assert.AreEqual("帳號及密碼不得為空", Login.Check("", "Password"));
+            Assert.AreEqual("帳號及密碼不得為空", Login.Check("account_for_tim", ""));
+            Assert.AreEqual("帳號及密碼不得為空", Login.Check("023545", ""));
+            Assert.AreEqual("帳號及密碼不得為空", Login.Check("", "15654561"));
+
+            //帳號密碼錯誤
+            Assert.AreEqual("帳號或密碼錯誤", Login.Check("account_for_tim", "12456156"));
+            Assert.AreEqual("帳號或密碼錯誤", Login.Check("asds15156156", "Password"));
         }
     }
 }
