3476df6e6b24e8f2cf7acf951df5f29ebd86a7ed
Jerry <l45632100@hotmail.com>
2015-06-05 23:00:50 +0800

    Revert "email varify new version"
    This reverts commit 1599390f49b6c765e4c8c70191c6af65b6511dce.
    
    Conflicts:
    	team4/UnitTest1.cs

--- team4/UnitTest1.cs
+++ team4/UnitTest1.cs
@@ -80,8 +80,13 @@ namespace team4
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	    // logout
 	    [TestMethod]
+=======
+	// logout
+	[TestMethod]
+>>>>>>> parent of 1599390... email varify new version
 =======
 	// logout
 	[TestMethod]
@@ -128,6 +133,7 @@ namespace team4
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         
         */ 
 
@@ -156,6 +162,10 @@ namespace team4
             Assert.AreEqual("帳號或密碼錯誤", Login.Check("account_for_tim", "12456156"));
             Assert.AreEqual("帳號或密碼錯誤", Login.Check("asds15156156", "Password"));
 
+=======
+
+        
+>>>>>>> parent of 1599390... email varify new version
 =======
 
         
